ed3859f30840806d537d19ac864cb88018003af1
JewishLewish <lanbrokhman@gmail.com>
2023-09-30 10:10:20 -0700

    Following, Followers, Link, etc.

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ class Bird(object):
     def __init__(self, target:str):
         self.target = target
         self.driver = webdriver.Chrome()
+        self.driver.get(self.target)
 
     def bio(self):
-        self.driver.get(self.target)
         bio = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[3]/div/div/span').text
         return bio
+    
+    def following(self):
+        f = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[5]/div[1]/a/span[1]/span').text
+        return f
 
-
+    def followers(self):
+        f = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[5]/div[2]/a/span[1]/span').text
+        return f
+    
+    def link(self):
+        f = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[4]/div/a/span').text
+        return f
 
 if __name__ == "__main__":
     site_url = 'https://twitter.com/Minecraft'
 
     bird = Bird(site_url)
 
-    bio = bird.bio()
-
-    print(bio)
+    print(bird.bio())
+    print(bird.following())
+    print(bird.followers())
+    print(bird.link())
