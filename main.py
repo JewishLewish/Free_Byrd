f73d1dae5f81d605f4db3e169d9a287ecd8b6de5
JewishLewish <lanbrokhman@gmail.com>
2023-10-04 19:15:09 -0700

    Debug main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     site_url = 'https://twitter.com/Minecraft'
     bird = Bird(site_url)
     bird.login()
-    time.sleep(1)
-    print(bird.bio())
+
+    while True:
+        True
